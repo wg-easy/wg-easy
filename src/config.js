49a2949aea79bc0da4a71e7d27b9af9a99dfd319
Fastidious <david@collantes.us>
2021-07-17 10:18:19 -0400

    Renaming and settings values
    Renaming environmental variable, and changing its default value.

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports.PASSWORD = process.env.PASSWORD;
 module.exports.WG_PATH = process.env.WG_PATH || '/etc/wireguard/';
 module.exports.WG_HOST = process.env.WG_HOST;
 module.exports.WG_PORT = process.env.WG_PORT || 51820;
-module.exports.WG_PERSISTENTKEEPALIVE = process.env.WG_PERSISTENTKEEPALIVE || 25;
+module.exports.WG_PERSISTENT_KEEPALIVE = process.env.WG_PERSISTENT_KEEPALIVEE || 0;
 module.exports.WG_DEFAULT_ADDRESS = process.env.WG_DEFAULT_ADDRESS || '10.8.0.x';
 module.exports.WG_DEFAULT_DNS = typeof process.env.WG_DEFAULT_DNS === 'string'
   ? process.env.WG_DEFAULT_DNS
