a55c9a5ef7133e1b3426cce58f45ce2a1811def2
Joshua K <22075247+joshuakraitberg@users.noreply.github.com>
2021-10-31 05:23:22 +0000

    Removed iptables rule configuration
    All iptables rules should be handled by `/etc/iptables/rules.v*`.  Rules cannot be trusted to be persisted otherwise.

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -65,10 +65,6 @@ module.exports = class WireGuard {
 
           throw err;
         });
-        // await Util.exec(`iptables -t nat -A POSTROUTING -s ${WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o eth0 -j MASQUERADE`);
-        // await Util.exec('iptables -A INPUT -p udp -m udp --dport 51820 -j ACCEPT');
-        // await Util.exec('iptables -A FORWARD -i wg0 -j ACCEPT');
-        // await Util.exec('iptables -A FORWARD -o wg0 -j ACCEPT');
         await this.__syncConfig();
 
         return config;
