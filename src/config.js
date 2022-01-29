05941cbda5c4134b3ecbe237688856630d660f67
Joel Heaps <13434824+joelheaps@users.noreply.github.com>
2022-01-28 20:50:21 -0600

    Update src/config.js
    Co-authored-by: crazyracer98 <8631139+crazyracer98@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -27,7 +27,7 @@ const modules = childProcess.execSync('lsmod', {
 })
 
 module.exports.WG_POST_UP = process.env.WG_POST_UP
-if (!!process.env.WG_POST_UP) {
+if (!process.env.WG_POST_UP) {
   module.exports.WG_POST_UP = `
   iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o eth0 -j MASQUERADE;
   iptables -A INPUT -p udp -m udp --dport 51820 -j ACCEPT;
