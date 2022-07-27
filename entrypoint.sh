14a50f3c036a95841bbb1a4023104d32a618b596
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-07-27 11:23:59 +0200

    Integrated coredns - logging what is being run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# run coredns on background
+# run CoreDNS in background
+echo "Running CoreDNS in background"
 cd /coredns && ./coredns &
 
-# run node
+# run app using node
+echo "Running app"
 cd /app && exec node server.js
