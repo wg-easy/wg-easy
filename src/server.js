25e7f45ae5e974952f60745b13db46003d636618
jerry.zhou <jerry.zhou@advantest.com>
2022-12-14 18:13:39 +0800

    Format the formatting in the server.js file.

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ require('./services/Server');
 
 const WireGuard = require('./services/WireGuard');
 
-WireGuard.getConfig()
-  .catch(err => {
+WireGuard.getConfig().catch((err) => {
   // eslint-disable-next-line no-console
-    console.error(err);
+  console.error(err);
 
-    // eslint-disable-next-line no-process-exit
-    process.exit(1);
-  });
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
