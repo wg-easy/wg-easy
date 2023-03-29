24f30be265484e83b81cf5910967a5e99e8eb57d
Matt Blu <mattdmasters@gmail.com>
2023-03-29 13:15:33 -0500

    Add DNS to server interface definition

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -100,6 +100,7 @@ PreUp = ${WG_PRE_UP}
 PostUp = ${WG_POST_UP}
 PreDown = ${WG_PRE_DOWN}
 PostDown = ${WG_POST_DOWN}
+${WG_DEFAULT_DNS ? `DNS = ${WG_DEFAULT_DNS}` : ''}
 `;
 
     for (const [clientId, client] of Object.entries(config.clients)) {
