c2b9cd46bb6e29186daab24aba00fb2d6505f0ef
Philip H <47042125+pheiduck@users.noreply.github.com>
2023-11-15 09:36:45 +0100

    Revert "fixup: Determine interface of default network rather than assume eth0"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { execSync } = require('child_process');
 const { release } = require('./package.json');
 
 module.exports.RELEASE = release;
@@ -8,7 +7,7 @@ module.exports.PORT = process.env.PORT || 51821;
 module.exports.WEBUI_HOST = process.env.WEBUI_HOST || '0.0.0.0';
 module.exports.PASSWORD = process.env.PASSWORD;
 module.exports.WG_PATH = process.env.WG_PATH || '/etc/wireguard/';
-module.exports.WG_DEVICE = process.env.WG_DEVICE || execSync("ip r | grep default | cut -d ' ' -f 5 | head -n1");
+module.exports.WG_DEVICE = process.env.WG_DEVICE || 'eth0';
 module.exports.WG_HOST = process.env.WG_HOST;
 module.exports.WG_PORT = process.env.WG_PORT || 51820;
 module.exports.WG_MTU = process.env.WG_MTU || null;
