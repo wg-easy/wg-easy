f5ad0da85d73c061d8431192423cb71858643aa2
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2024-01-12 00:24:12 +0300

    Initialize new Vue project; move existing contents

---
+++ src/webui/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
