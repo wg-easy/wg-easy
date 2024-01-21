f31f8d48984c056746a78ec465bb7e688bc259e2
Philip H <47042125+pheiduck@users.noreply.github.com>
2024-01-21 11:57:50 +0100

    revert: AllowedIPs = ${allowedIPs}`;

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -126,7 +126,7 @@ PostDown = ${WG_POST_DOWN}
 [Peer]
 PublicKey = ${client.publicKey}
 ${client.preSharedKey ? `PresharedKey = ${client.preSharedKey}\n` : ''
-}AllowedIPs = ${client.address}/32`;
+}AllowedIPs = ${allowedIPs}`;
     }
 
     debug('Config saving...');
