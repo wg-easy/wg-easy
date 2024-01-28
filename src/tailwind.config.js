20ae02dd48a9884de20869b31be9cd8260b6b8b5
Thomas Willems <twillems@willtho.com>
2024-01-28 11:14:17 +0100

    add .is-disabled

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,4 +5,15 @@
 module.exports = {
   darkMode: 'media',
   content: ['./www/**/*.{html,js}'],
+  plugins: [
+    function addDisabledClass({ addUtilities }) {
+      const newUtilities = {
+        '.is-disabled': {
+          opacity: '0.25',
+          cursor: 'default',
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
