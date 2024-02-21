38ff006a2b17392a70604cda468f13defeece81b
Chanchai <chanchai@localhost.local>
2024-02-21 13:56:39 +0700

    feat: support graceful shutdown

--- src/server.js
+++ src/server.js
@@ -12,3 +12,15 @@ WireGuard.getConfig()
     // eslint-disable-next-line no-process-exit
     process.exit(1);
   });
+
+// Handle terminate signal
+process.on('SIGTERM', async() => {
+  console.log('SIGTERM signal received.');
+  await WireGuard.Shutdown();
+  process.exit(0);
+});
+
+// Handle interupt signal
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received.');
+});
