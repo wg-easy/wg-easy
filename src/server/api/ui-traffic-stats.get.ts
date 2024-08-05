3040bd4c9be48ae01293c76001749b961675af7e
Bernd Storath <999999bst@gmail.com>
2024-08-05 21:52:44 +0200

    fix error

--- src/server/api/ui-traffic-stats.get.ts
+++ src/server/api/ui-traffic-stats.get.ts
@@ -1,4 +1,6 @@
 export default defineEventHandler((event) => {
   setHeader(event, 'Content-Type', 'application/json');
-  return UI_TRAFFIC_STATS === 'true' ? true : false;
+  // Weird issue with auto import not working. This alias is needed
+  const stats = UI_TRAFFIC_STATS;
+  return stats === 'true' ? true : false;
 });
