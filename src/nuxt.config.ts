80e9a1388ab6a43044eb25cb5a9ef0f2a44c44ea
Bernd Storath <999999bst@gmail.com>
2024-08-14 11:54:10 +0200

    use nuxt 4

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
