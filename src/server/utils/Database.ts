d153ba67eb7e39ed00cca53b8d49979deb6a23a1
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-09-01 09:10:03 +0200

    add database (#1330)
    * add: database abstraction
    
    * update: get lang from database
    
    * udpate: with repositories
    
    * add: interfaces to connect a database provider
    
    - easy swapping between database provider
    
    * add: setup page
    
    - add: in-memory database provider
    - create a new account (signup)
    - login with username and password
    - first setup page to create an account
    - PASSWORD_HASH was removed from environment and files was updated/removed due to that change
    
    * update: Dockerfile
    
    * fix: review done
    
    - remove: REQUIRES_PASSWORD & RELEASE environment variables
    
    * fix: i18n translation
    
    - rename directories
    
    * update: use database
    
    * fix: typecheck
    
    * fix: review
    
    * rebase & add: persistent lowdb provider
    
    * update: french translation
    
    * revert: due to rebase
    
    * remove & document

---
+++ src/server/utils/Database.ts
@@ -0,0 +1,17 @@
+/**
+ * Changing the Database Provider
+ * This design allows for easy swapping of different database implementations.
+ *
+ */
+
+// import InMemory from '~/services/database/inmemory';
+import LowDb from '~/services/database/lowdb';
+
+const provider = new LowDb();
+
+provider.connect().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+export default provider;
