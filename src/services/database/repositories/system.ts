0163f7f4c108524a53eb6875c3533c54bd7a9ea2
Bernd Storath <32197462+kaaax0815@users.noreply.github.com>
2024-09-03 10:56:06 +0200

    Feat: Migrations (#1344)
    * add migrations
    
    * improve migration runner
    
    * improve migration runner
    
    * document what each migration does

--- src/services/database/repositories/system.ts
+++ src/services/database/repositories/system.ts
@@ -1,7 +1,6 @@
-import packageJson from '@@/package.json';
-
 import type { SessionConfig } from 'h3';
-import type { Lang } from './types';
+
+export type Lang = 'en' | 'fr';
 
 export type IpTables = {
   PreUp: string;
@@ -84,63 +83,5 @@ export interface SystemRepository {
   /**
    * Retrieves the system configuration data from the database.
    */
-  getSystem(): Promise<System | null>;
-
-  /**
-   * Retrieves the system's language setting.
-   */
-  getLang(): Promise<Lang>;
+  getSystem(): Promise<System>;
 }
-
-// TODO: move to migration
-export const DEFAULT_SYSTEM: System = {
-  release: packageJson.release.version,
-  interface: {
-    privateKey: '',
-    publicKey: '',
-    address: '10.8.0.1',
-  },
-  sessionTimeout: 3600, // 1 hour
-  lang: 'en',
-  userConfig: {
-    mtu: 1420,
-    persistentKeepalive: 0,
-    // TODO: assume handle CIDR to compute next ip in WireGuard
-    rangeAddress: '10.8.0.0/24',
-    defaultDns: ['1.1.1.1'],
-    allowedIps: ['0.0.0.0/0', '::/0'],
-  },
-  wgPath: WG_PATH,
-  wgDevice: 'wg0',
-  wgHost: WG_HOST || '',
-  wgPort: 51820,
-  wgConfigPort: 51820,
-  iptables: {
-    PreUp: '',
-    PostUp: '',
-    PreDown: '',
-    PostDown: '',
-  },
-  trafficStats: {
-    enabled: false,
-    type: ChartType.None,
-  },
-  clientExpiration: {
-    enabled: false,
-  },
-  oneTimeLinks: {
-    enabled: false,
-  },
-  sortClients: {
-    enabled: false,
-  },
-  prometheus: {
-    enabled: false,
-    password: null,
-  },
-  sessionConfig: {
-    password: getRandomHex(256),
-    name: 'wg-easy',
-    cookie: undefined,
-  },
-};
