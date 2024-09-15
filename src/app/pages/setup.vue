6f41abe652ee2cec1bf6282f8b49a657145e8169
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-09-15 15:44:37 +0200

    update: setup page error handle
    - use fetch error data to provide error message
    - use translation to provider error message

--- src/app/pages/setup.vue
+++ src/app/pages/setup.vue
@@ -28,16 +28,12 @@
             autocomplete="username"
             :placeholder="$t('setup.usernamePlaceholder')"
             class="px-3 py-2 text-sm dark:bg-neutral-700 text-gray-500 dark:text-gray-500 mb-5 border-2 border-gray-100 dark:border-neutral-800 rounded-lg w-full focus:border-red-800 dark:focus:border-red-800 dark:placeholder:text-neutral-400 outline-none"
-            required="true"
           />
-          <small v-if="errorCU" class="text-danger">{{
-            $t('setup.usernameCondition')
-          }}</small>
         </div>
         <div>
-          <label for="password" class="inline-block py-2">{{
+          <Label for="password" class="inline-block py-2">{{
             $t('setup.newPassword')
-          }}</label>
+          }}</Label>
           <input
             id="password"
             v-model="password"
@@ -46,17 +42,13 @@
             autocomplete="new-password"
             :placeholder="$t('setup.passwordPlaceholder')"
             class="px-3 py-2 text-sm dark:bg-neutral-700 text-gray-500 dark:text-gray-500 mb-5 border-2 border-gray-100 dark:border-neutral-800 rounded-lg w-full focus:border-red-800 dark:focus:border-red-800 dark:placeholder:text-neutral-400 outline-none"
-            required="true"
           />
-          <small v-if="errorPWD" class="text-danger">{{
-            $t('setup.passwordCondition')
-          }}</small>
         </div>
         <div>
-          <label for="accept" class="inline-block my-4 mr-4">{{
+          <Label for="accept" class="inline-block my-4 mr-4">{{
             $t('setup.accept')
-          }}</label>
-          <input id="accept" type="checkbox" name="accept" required="true" />
+          }}</Label>
+          <input id="accept" type="checkbox" name="accept" />
         </div>
         <button
           type="submit"
@@ -74,16 +66,38 @@
         </button>
       </form>
     </div>
+
+    <ErrorToast
+      v-if="setupError"
+      :title="setupError.title"
+      :message="setupError.message"
+      :duration="12000"
+    />
   </main>
 </template>
 
 <script setup lang="ts">
+import { FetchError } from 'ofetch';
+
+const { t } = useI18n();
 const username = ref<null | string>(null);
 const password = ref<null | string>(null);
 const authStore = useAuthStore();
 
-const errorCU = ref(false);
-const errorPWD = ref(false);
+type SetupError = {
+  title: string;
+  message: string;
+};
+
+const setupError = ref<null | SetupError>(null);
+
+watch(setupError, (value) => {
+  if (value) {
+    setTimeout(() => {
+      setupError.value = null;
+    }, 6000);
+  }
+});
 
 async function newAccount() {
   if (!username.value || !password.value) return;
@@ -94,10 +108,11 @@ async function newAccount() {
       navigateTo('/login');
     }
   } catch (error) {
-    if (error instanceof Error) {
-      // TODO: replace alert with actual ui error message
-      // TODO: also use errorCU & errorPWD to show prompt error
-      alert(error.message || error.toString());
+    if (error instanceof FetchError) {
+      setupError.value = {
+        title: t('setup.requirements'),
+        message: error.data.message,
+      };
     }
   }
 }
