edf55835c8dedb6a6f124d12f955c1049d1464cf
Michail Montesantos <michail.montesantos@gmail.com>
2024-12-15 22:25:03 +0100

    add WG_SERVER_MTU to config.js, enable MTU only if mtu is specified

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ module.exports.WG_PATH = process.env.WG_PATH || '/etc/wireguard/';
 module.exports.WG_DEVICE = process.env.WG_DEVICE || 'eth0';
 module.exports.WG_HOST = process.env.WG_HOST;
 module.exports.WG_PORT = process.env.WG_PORT || '51820';
+module.exports.WG_SERVER_MTU = process.env.WG_SERVER_MTU;
 module.exports.WG_CONFIG_PORT = process.env.WG_CONFIG_PORT || process.env.WG_PORT || '51820';
 module.exports.WG_MTU = process.env.WG_MTU || null;
 module.exports.WG_PERSISTENT_KEEPALIVE = process.env.WG_PERSISTENT_KEEPALIVE || '0';
