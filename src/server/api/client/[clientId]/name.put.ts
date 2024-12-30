34c899bdc763234b2d96b93f7b380c2431d8a387
Bernd Storath <999999bst@gmail.com>
2024-12-30 14:52:22 +0100

    remove routes, fix config

--- src/server/api/client/[clientId]/name.put.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const { clientId } = await getValidatedRouterParams(
-    event,
-    validateZod(clientIdType)
-  );
-  const { name } = await readValidatedBody(event, validateZod(nameType));
-  await WireGuard.updateClientName({ clientId, name });
-  return { success: true };
-});
