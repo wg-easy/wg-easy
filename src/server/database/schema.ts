c24da66dadcd2df7a5b41d10963441c48af0cd8c
Bernd Storath <999999bst@gmail.com>
2025-01-17 08:51:35 +0100

    improve docker dev
    only install dependencies if changed

--- src/server/database/schema.ts
+++ src/server/database/schema.ts
@@ -1,6 +1,5 @@
 // Make sure to not use any Path Aliases in these files
 export * from './repositories/client/schema';
-export * from './repositories/general/schema';
 export * from './repositories/hooks/schema';
 export * from './repositories/interface/schema';
 export * from './repositories/metrics/schema';
