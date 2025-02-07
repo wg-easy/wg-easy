f0bbf51ffdc980e8ed70087f6f69363a3cebf70e
Anyexyz <anyexyz@foxmail.com>
2025-02-08 07:43:00 +0800

    Feat: Add Chinese language support

--- src/i18n/i18n.config.ts
+++ src/i18n/i18n.config.ts
@@ -1,9 +1,11 @@
 import en from './locales/en.json';
+import zh_cn from './locales/zh-cn.json';
 
 export default defineI18nConfig(() => ({
   legacy: false,
   fallbackLocale: 'en',
   messages: {
     en,
+    zh_cn,
   },
 }));
