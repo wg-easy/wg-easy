2dc7ba6708a860177bd2d0c8a691dda72a3fced4
Bernd Storath <999999bst@gmail.com>
2025-02-12 09:34:57 +0100

    show error for old env vars

--- src/server/utils/config.ts
+++ src/server/utils/config.ts
@@ -4,3 +4,10 @@ import packageJson from '@@/package.json';
 export const RELEASE = 'v' + packageJson.version;
 
 export const SERVER_DEBUG = debug('Server');
+
+export const OLD_ENV = {
+  /** @deprecated Only for migration purposes */
+  PASSWORD: process.env.PASSWORD,
+  /** @deprecated Only for migration purposes */
+  PASSWORD_HASH: process.env.PASSWORD_HASH,
+};
