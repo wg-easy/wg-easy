302c332a85c2bfa833e78a4c3f15d0f13130087d
Bernd Storath <999999bst@gmail.com>
2025-03-03 08:50:01 +0100

    fix type errors

--- src/app/components/Form/Label.vue
+++ src/app/components/Form/Label.vue
@@ -1,9 +1,11 @@
 <template>
-  <RLabel :for="id" class="md:align-middle md:leading-10"><slot /></RLabel>
+  <RLabel :for="props.for" class="md:align-middle md:leading-10"
+    ><slot
+  /></RLabel>
 </template>
 
 <script lang="ts" setup>
 import { Label as RLabel } from 'radix-vue';
 
-defineProps<{ id: string }>();
+const props = defineProps<{ for: string }>();
 </script>
