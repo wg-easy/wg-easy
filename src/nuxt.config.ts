25a3f5af3ad94c08f182f07da01068875460355a
Bernd Storath <999999bst@gmail.com>
2025-03-04 11:32:26 +0100

    update date

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-02-04',
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/i18n',
