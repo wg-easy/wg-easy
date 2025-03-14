49ba05ddd3cd66c8409c171f6053381cd1b72b12
Bernd Storath <999999bst@gmail.com>
2025-03-14 08:40:03 +0100

    get private info

--- src/server/utils/ip.ts
+++ src/server/utils/ip.ts
@@ -1,4 +1,5 @@
 import { Resolver } from 'node:dns/promises';
+import { networkInterfaces } from 'node:os';
 import { stringifyIp } from 'ip-bigint';
 import type { parseCidr } from 'cidr-tools';
 
@@ -40,7 +41,7 @@ const dnsServers = {
   ip: 'myip.opendns.com',
 };
 
-export async function getPublicInformation() {
+async function getPublicInformation() {
   const ipv4 = await getPublicIpv4();
   const ipv6 = await getPublicIpv6();
 
@@ -83,3 +84,89 @@ async function getReverseDns(ip: string) {
     return [];
   }
 }
+
+function getPrivateInformation() {
+  const interfaces = networkInterfaces();
+
+  const interfaceNames = Object.keys(interfaces);
+
+  const obj: Record<string, { ipv4: string[]; ipv6: string[] }> = {};
+
+  for (const name of interfaceNames) {
+    if (name === 'wg0') {
+      continue;
+    }
+
+    const iface = interfaces[name];
+    if (!iface) continue;
+
+    for (const { family, internal, address } of iface) {
+      if (internal) {
+        continue;
+      }
+      if (!(name in obj)) {
+        obj[name] = {
+          ipv4: [],
+          ipv6: [],
+        };
+      }
+      if (family === 'IPv4') {
+        obj[name].ipv4.push(address);
+      } else if (family === 'IPv6') {
+        obj[name].ipv6.push(address);
+      }
+    }
+  }
+
+  return obj;
+}
+
+async function getIpInformation() {
+  const results = [];
+
+  const publicInfo = await getPublicInformation();
+  if (publicInfo.ipv4) {
+    results.push({
+      value: publicInfo.ipv4,
+      label: 'IPv4 - Public',
+    });
+  }
+  if (publicInfo.ipv6) {
+    results.push({
+      value: `[${publicInfo.ipv6}]`,
+      label: 'IPv6 - Public',
+    });
+  }
+  for (const hostname of publicInfo.hostnames) {
+    results.push({
+      value: hostname,
+      label: 'Hostname - Public',
+    });
+  }
+
+  const privateInfo = getPrivateInformation();
+  for (const [name, { ipv4, ipv6 }] of Object.entries(privateInfo)) {
+    for (const ip of ipv4) {
+      results.push({
+        value: ip,
+        label: `IPv4 - ${name}`,
+      });
+    }
+    for (const ip of ipv6) {
+      results.push({
+        value: `[${ip}]`,
+        label: `IPv6 - ${name}`,
+      });
+    }
+  }
+
+  return results;
+}
+
+/**
+ * Fetch IP Information
+ * @cache Response is cached for 15 min
+ */
+export const cachedGetIpInformation = cacheFunction(getIpInformation, {
+  expiry: 15 * 60 * 1000,
+});
