a6e33694750d1aaf5c2456d0eb4ca22d4b6e8645
Pokydko Oleksandr <32617699+Shuna322@users.noreply.github.com>
2025-03-16 16:47:19 +0200

    Fix: Improve special character regex (#1744) * update password special character regex to support ( `-` `_` `=` `+` `[` `]` `;` `'` `\` `/` )

--- src/server/database/repositories/user/types.ts
+++ src/server/database/repositories/user/types.ts
@@ -15,7 +15,7 @@ const password = z
   .regex(/[A-Z]/, t('zod.user.passwordUppercase'))
   .regex(/[a-z]/, t('zod.user.passwordLowercase'))
   .regex(/\d/, t('zod.user.passwordNumber'))
-  .regex(/[!@#$%^&*(),.?":{}|<>]/, t('zod.user.passwordSpecial'))
+  .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]/, t('zod.user.passwordSpecial'))
   .pipe(safeStringRefine);
 
 const remember = z.boolean({ message: t('zod.user.remember') });
