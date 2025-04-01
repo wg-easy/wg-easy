c7fb8bfd82607a1d4469a66adf54693fc0a85816
Bernd Storath <999999bst@gmail.com>
2025-04-01 15:01:24 +0200

    add startup info

--- src/server/plugins/manager.ts
+++ src/server/plugins/manager.ts
@@ -1,4 +1,12 @@
 export default defineNitroPlugin((nitroApp) => {
+  console.log(`====================================================`);
+  console.log(`    wg-easy - https://github.com/wg-easy/wg-easy    `);
+  console.log(`====================================================`);
+  console.log(`| wg-easy:  ${RELEASE.padEnd(38)} |`);
+  console.log(`| Node:     ${process.version.padEnd(38)} |`);
+  console.log(`| Platform: ${process.platform.padEnd(38)} |`);
+  console.log(`| Arch:     ${process.arch.padEnd(38)} |`);
+  console.log(`====================================================`);
   nitroApp.hooks.hook('close', async () => {
     console.log('Shutting down');
     await WireGuard.Shutdown();
