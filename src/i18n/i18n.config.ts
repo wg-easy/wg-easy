77b4f9db6576fbcda2e10af600d8775c2663ddf4
Nikolas <160475670+Loudhot@users.noreply.github.com>
2025-06-01 17:40:31 +0300

    Added Ukrainian language (#1906)
    * Add files via upload
    
    Ukrainian language
    
    * Update ua.json
    
    * Update i18n.config.ts
    
    * Update i18n.config.ts
    
    * Rename ua.json to uk.json
    
    * Update i18n.config.ts
    
    * Update nuxt.config.ts
    
    * Update uk.json

--- src/i18n/i18n.config.ts
+++ src/i18n/i18n.config.ts
@@ -1,9 +1,11 @@
 import en from './locales/en.json';
+import uk from './locales/uk.json';
 
 export default defineI18nConfig(() => ({
   legacy: false,
   fallbackLocale: 'en',
   messages: {
     en,
+    uk,
   },
 }));
