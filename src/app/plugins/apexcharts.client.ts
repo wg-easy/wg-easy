bbc4232cc3e3f6faf31ce5a51337310290111585
Philip H. <47042125+pheiduck@users.noreply.github.com>
2025-06-22 06:13:17 +0200

    fixes apexcharts.client.ts
    some errors of th Linter

--- src/app/plugins/apexcharts.client.ts
+++ src/app/plugins/apexcharts.client.ts
@@ -2,4 +2,5 @@ import VueApexCharts from 'vue3-apexcharts';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueApexCharts);
+  nuxtApp.vueApp.component("ApexChart", VueApexCharts);
 });
