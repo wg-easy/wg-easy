439a2214d818e17a02f5aa3e29702667d530e78d
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2025-09-15 18:59:20 +0200

    Add INIT_ALLOWED_IPS env var
    Implement INIT_ALLOWED_IPS env var like the INIT_DNS to preset the global Allowed IPs field.

--- src/server/database/sqlite.ts
+++ src/server/database/sqlite.ts
@@ -96,6 +96,15 @@ async function initialSetup(db: DBServiceType) {
     });
   }
 
+  if (WG_INITIAL_ENV.ALLOWED_IPS) {
+    DB_DEBUG('Setting initial Allowed IPs...');
+    const userConfig = await db.userConfigs.get();
+    await db.userConfigs.update({
+      ...userConfig,
+      defaultAllowedIps: WG_INITIAL_ENV.ALLOWED_IPS,
+    });
+  }
+
   if (
     WG_INITIAL_ENV.USERNAME &&
     WG_INITIAL_ENV.PASSWORD &&
