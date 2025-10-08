wg-easy/wg-easy

commit 07f89d15a9835e7732a866f5a771088b247b7c29

Feat: show client endpoint (#2058)
* show client endpoint

* improve

* fix status code

src/server/api/client/[clientId]/index.get.ts
@@ -18,6 +18,18 @@ export default definePermissionEventHandler(
         statusMessage: 'Client not found',
       });
     }
-    return result;
+
+    const data = await WireGuard.dumpByPublicKey(result.publicKey);
+    if (!data) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'Failed to dump client data',
+      });
+    }
+
+    return {
+      ...result,
+      endpoint: data.endpoint,
+    };
   }
 );

src/app/components/Form/InfoField.vue
@@ -1,0 +1,20 @@
+<template>
+  <div class="flex items-center">
+    <FormLabel :for="id">
+      {{ label }}
+    </FormLabel>
+    <BaseTooltip v-if="description" :text="description">
+      <IconsInfo class="size-4" />
+    </BaseTooltip>
+  </div>
+  <span :id="id">{{ data }}</span>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  id: string;
+  label: string;
+  description?: string;
+  data?: string;
+}>();
+</script>

src/app/pages/clients/[id].vue
@@ -39,6 +39,12 @@
               v-model="data.ipv6Address"
               label="IPv6"
             />
+            <FormInfoField
+              id="endpoint"
+              :data="data.endpoint ?? $t('client.notConnected')"
+              :label="$t('client.endpoint')"
+              :description="$t('client.endpointDesc')"
+            />
           </FormGroup>
           <FormGroup>
             <FormHeading :description="$t('client.allowedIpsDesc')">

src/server/utils/WireGuard.ts
@@ -93,6 +93,17 @@ class WireGuard {
     return clients;
   }
 
+  async dumpByPublicKey(publicKey: string) {
+    const wgInterface = await Database.interfaces.get();
+
+    const dump = await wg.dump(wgInterface.name);
+    const clientDump = dump.find(
+      ({ publicKey: dumpPublicKey }) => dumpPublicKey === publicKey
+    );
+
+    return clientDump;
+  }
+
   async getAllClients() {
     const wgInterface = await Database.interfaces.get();
     const dbClients = await Database.clients.getAllPublic();

commit 529d65b3fbd877be66d7f22fe3e90b185c7ede73

Fix: don't expect dump to contain client
Fixes: #2200
Fixes Bug introduced in: #2058

src/server/api/client/[clientId]/index.get.ts
@@ -19,17 +19,12 @@ export default definePermissionEventHandler(
       });
     }
 
+    // data can be undefined if the client is disabled
     const data = await WireGuard.dumpByPublicKey(result.publicKey);
-    if (!data) {
-      throw createError({
-        statusCode: 500,
-        statusMessage: 'Failed to dump client data',
-      });
-    }
 
     return {
       ...result,
-      endpoint: data.endpoint,
+      endpoint: data?.endpoint,
     };
   }
 );