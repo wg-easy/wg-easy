04f0dc20217ee495d32dfea1e05dd40c3002a04f
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2026-06-03 14:34:45 +0700

    Move the bypass logic back to update checking function

--- src/server/api/information.get.ts
+++ src/server/api/information.get.ts
@@ -1,13 +1,7 @@
 import { gt } from 'semver';
 
 export default defineEventHandler(async () => {
-  let latestRelease: Awaited<ReturnType<typeof cachedFetchLatestRelease>>;
-  if (WG_ENV.DISABLE_VERSION_CHECK) {
-    latestRelease = { version: RELEASE, changelog: '' };
-  } else {
-    latestRelease = await cachedFetchLatestRelease();
-  }
-
+  const latestRelease = await cachedFetchLatestRelease();
   const updateAvailable = gt(latestRelease.version, RELEASE);
   const insecure = WG_ENV.INSECURE;
   const isAwg = WG_ENV.WG_EXECUTABLE === 'awg';
