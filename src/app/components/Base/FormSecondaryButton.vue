2a8832dbef58280cc931ff251e670a7da8078649
anhhna <anhhna@gmail.com>
2026-06-13 12:05:04 -0500

    fix: button Delete & View Configuration on Edit Client too small

--- src/app/components/Base/FormSecondaryButton.vue
+++ src/app/components/Base/FormSecondaryButton.vue
@@ -2,7 +2,7 @@
   <component
     :is="elementType"
     role="button"
-    class="rounded-lg border-2 border-gray-100 text-gray-500 hover:border-red-800 hover:bg-red-800 hover:text-white focus:border-red-800 focus:outline-0 focus:ring-0 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-100 disabled:text-gray-400 dark:border-neutral-800 dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-400 dark:disabled:border-neutral-800 dark:disabled:bg-neutral-700 dark:disabled:text-neutral-400"
+    class="rounded-lg border-2 border-gray-100 px-4 py-2 text-gray-500 hover:border-red-800 hover:bg-red-800 hover:text-white focus:border-red-800 focus:outline-0 focus:ring-0 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-100 disabled:text-gray-400 dark:border-neutral-800 dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-400 dark:disabled:border-neutral-800 dark:disabled:bg-neutral-700 dark:disabled:text-neutral-400"
     v-bind="attrs"
   >
     <slot />
